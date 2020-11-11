e8c88a0cfb2c6d62f81974d79097920c9c508d41
jodec <jodec@DESKTOP-GKH7NGN>
2020-11-11 12:41:02 -0500

    PP 12.8

--- Assignments/src/com/cs122/assignments/ProgrammingProjects12_8.java
+++ Assignments/src/com/cs122/assignments/ProgrammingProjects12_8.java
@@ -1,41 +1,50 @@
 package com.cs122.assignments;
 
+import java.util.Scanner;
+
 public class ProgrammingProjects12_8 {
 	
 
-	public static int calcPascal(int n, int c) {
+	public static int calcPascal(int r, int c) {
 		int result = 0;
-		if((c == 0) || c == n) {
+		if((c == 0) || c == r) {
 			result = 1;
 		}
 		else
-			result = calcPascal(n-1, c-1) + calcPascal(n-1, c);
+			result = calcPascal(r-1, c-1) + calcPascal(r-1, c);
 		
 		return result;
 	}
-	public static int[] pascal(int n) {
-		int[][] triangle = new int[n][];
-		int[] row = new int[n];
-		int count = 0;
-		if(count == 0 || count == n-1) {
-			row[count] = 1;
-			count++;
-		}
-		else {
-			while(count < n-1) {
-				row[count] = pascal()
-				triangle[n-1][count] = row[count];
-				count++;
-			}
-			
-		}
-		return row;
-	}
+	
 	
 	public static void main(String[] args) {
-		int num = 4;
-		for(int i = 0; i < num; i++) {
-			System.out.print(pascal(num)[i] + "  ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pascal line: ");
+		int n = sc.nextInt();
+		
+		String[] triangle = new String[n];
+		String row = "";
+		for(int r=0; r<n; r++) {
+			for(int c=0; c<r+1; c++) {
+				row += calcPascal(r,c) + " ";
+				if(c == r) {
+					triangle[r] = row;
+					row = "";
+				}
+			}
 		}
+		System.out.println(triangle[n-1]);
+		
+/*		THIS IS USED TO PRINT THE WHOLE TRIANGLE    */
+//		for(int r=0; r<n; r++) {			
+//			for(int c=0; c<r+1; c++) {
+//				System.out.print(calcPascal(r,c) + " ");
+//				if(c == r) {
+//					System.out.println();
+//				}
+//			}
+//		}
 	}
+	
 }
+
